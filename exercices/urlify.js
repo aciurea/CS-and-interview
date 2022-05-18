14a76183d2c578b5f34b70b796aac9f9bd456419
alin.ciurea <alin.ciurea@siiromania.ro>
2022-05-18 13:08:25 +0300

    Added space at the end.

--- exercices/urlify.js
+++ exercices/urlify.js
@@ -1,13 +1,14 @@
-const urlify = (str) => {
+//@ts-check
+const urlify = (str = '') => {
   let newStr = '';
-  for (const char of str) {
+  for (const char of str.trimEnd()) {
     newStr += char === ' ' ? '%20' : char;
   }
 
   return newStr;
 };
 
-const urlify2 = (str) => str.split(' ').join('%20');
+const urlify2 = (str) => str.trimEnd().split(' ').join('%20');
 
-console.log(urlify('abc d e f g'));
-console.log(urlify2('abc d e f g'));
+console.log(urlify('abc d e f g     '));
+console.log(urlify2('abc d e f g    '));
