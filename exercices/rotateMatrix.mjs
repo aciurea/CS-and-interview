476ead14c5710a8ba7a63497d155235b2e73843b
alin.ciurea <alin.ciurea@siiromania.ro>
2022-05-27 11:21:16 +0300

    Invalidate the row, refactored the logging of matrix. Reverse number

---
+++ exercices/rotateMatrix.mjs
@@ -0,0 +1,26 @@
+import { logMatrix } from './log.mjs';
+
+function rotateMatrix(matrix) {
+  const length = matrix[0].length - 1;
+  const newMatrix = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+  ];
+
+  for (let row = 0; row <= length; row++) {
+    for (let col = 0; col <= length; col++) {
+      newMatrix[length - col][row] = matrix[row][col];
+    }
+  }
+  return newMatrix;
+}
+
+const matrix = [
+  ['a', 'b', 'c'],
+  ['d', 'e', 'f'],
+  ['g', 'h', 'i'],
+];
+
+const expectedMatrix = rotateMatrix(matrix);
+logMatrix(expectedMatrix);
